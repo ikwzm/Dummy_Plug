e4001005fd53576b1078d1222cf89c408a5fdbec
KAWAZOME Ichiro <ichiro_k@ca2.so-net.ne.jp>
2014-03-10 02:03:49 +0900

    AXI4_SLAVE_PLAYERで、アドレスの転送と１バーストのライトデータ転送が同時に発生した場合に、応答を返さない不具合を修正した.

--- src/main/vhdl/axi4/axi4_channel_player.vhd
+++ src/main/vhdl/axi4/axi4_channel_player.vhd
@@ -1,8 +1,8 @@
 -----------------------------------------------------------------------------------
 --!     @file    axi4_channel_player.vhd
 --!     @brief   AXI4 A/R/W/B Channel Dummy Plug Player.
---!     @version 1.5.2
---!     @date    2014/3/1
+--!     @version 1.5.3
+--!     @date    2014/3/10
 --!     @author  Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
 -----------------------------------------------------------------------------------
 --
@@ -3051,7 +3051,9 @@ begin
                     proc_name => proc_name, 
                     signals   => out_signals.B
                 );
-                wait_until_xfer_w (core, proc_name, timeout, '1');
+                if (not (WVALID_I = '1' and WREADY_I = '1' and WLAST_I = '1')) then
+                    wait_until_xfer_w (core, proc_name, timeout, '1');
+                end if;
                 execute_output(out_signals);
                 wait_until_xfer_b (core, proc_name, timeout);
                 out_signals.B := AXI4_B_CHANNEL_SIGNAL_NULL;
