7b9347109b0a3f54318412c15d79f0c28d60e5d2
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2024-04-15 13:28:06 +0900

    [update] for Release 1.9.2

--- src/main/vhdl/axi4/axi4_models.vhd
+++ src/main/vhdl/axi4/axi4_models.vhd
@@ -1,13 +1,13 @@
 -----------------------------------------------------------------------------------
 --!     @file    axi4_models.vhd                                                 --
 --!     @brief   AXI4 Dummy Plug Component Package                               --
---!     @version 1.9.1                                                           --
---!     @date    2023/12/12                                                      --
+--!     @version 1.9.2                                                           --
+--!     @date    2024/04/15                                                      --
 --!     @author  Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>                     --
 -----------------------------------------------------------------------------------
 -----------------------------------------------------------------------------------
 --                                                                               --
---      Copyright (C) 2023 Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>           --
+--      Copyright (C) 2024 Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>           --
 --      All rights reserved.                                                     --
 --                                                                               --
 --      Redistribution and use in source and binary forms, with or without       --
--- src/main/vhdl/axi4/axi4_stream_slave_player.vhd
+++ src/main/vhdl/axi4/axi4_stream_slave_player.vhd
@@ -1,12 +1,12 @@
 -----------------------------------------------------------------------------------
 --!     @file    axi4_stream_slave player.vhd
 --!     @brief   AXI4-Stream Slave Dummy Plug Player.
---!     @version 1.9.1
---!     @date    2023/12/12
+--!     @version 1.9.2
+--!     @date    2024/4/15
 --!     @author  Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
 -----------------------------------------------------------------------------------
 --
---      Copyright (C) 2012-2023 Ichiro Kawazome
+--      Copyright (C) 2012-2024 Ichiro Kawazome
 --      All rights reserved.
 --
 --      Redistribution and use in source and binary forms, with or without
