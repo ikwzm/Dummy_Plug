69b8d272c8bde2b3f54bc5fef811599c4e3391d8
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2025-05-06 15:09:09 +0900

    [update] for Release 1.9.3

--- src/main/vhdl/axi4/axi4_models.vhd
+++ src/main/vhdl/axi4/axi4_models.vhd
@@ -1,13 +1,13 @@
 -----------------------------------------------------------------------------------
 --!     @file    axi4_models.vhd                                                 --
 --!     @brief   AXI4 Dummy Plug Component Package                               --
---!     @version 1.9.2                                                           --
---!     @date    2024/04/15                                                      --
+--!     @version 1.9.3                                                           --
+--!     @date    2025/05/06                                                      --
 --!     @author  Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>                     --
 -----------------------------------------------------------------------------------
 -----------------------------------------------------------------------------------
 --                                                                               --
---      Copyright (C) 2024 Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>           --
+--      Copyright (C) 2025 Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>           --
 --      All rights reserved.                                                     --
 --                                                                               --
 --      Redistribution and use in source and binary forms, with or without       --
--- src/main/vhdl/axi4/axi4_types.vhd
+++ src/main/vhdl/axi4/axi4_types.vhd
@@ -1,12 +1,12 @@
 -----------------------------------------------------------------------------------
 --!     @file    axi4_types.vhd
 --!     @brief   AXI4 Channel Signal Type Package.
---!     @version 1.9.2
---!     @date    2024/4/15
+--!     @version 1.9.3
+--!     @date    2025/5/6
 --!     @author  Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
 -----------------------------------------------------------------------------------
 --
---      Copyright (C) 2012-2024 Ichiro Kawazome
+--      Copyright (C) 2012-2025 Ichiro Kawazome
 --      All rights reserved.
 --
 --      Redistribution and use in source and binary forms, with or without
